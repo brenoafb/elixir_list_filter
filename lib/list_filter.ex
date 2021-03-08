1e60fee7dd26e6f769a2ce60c775d12be15ff368
brenoafb <breno.afb@gmail.com>
2021-03-08 19:24:04 -0300

    Add solution

--- lib/list_filter.ex
+++ lib/list_filter.ex
@@ -3,16 +3,37 @@ defmodule ListFilter do
   Documentation for `ListFilter`.
   """
 
+  require Integer
+
   @doc """
-  Hello world.
+  Given a list of strings, return how many elements correspond
+  to an odd number.
 
   ## Examples
 
-      iex> ListFilter.hello()
-      :world
+      iex> ListFilter.call(["1", "3", "6", "43", "banana", "6", "abc"])
+      3
+      iex> ListFilter.call(["2", "4", "banana"])
+      0
 
   """
-  def hello do
-    :world
+  def call(list) do
+    list
+    |> Enum.map(fn x -> read_integer(x) end)
+    |> Enum.filter(fn x -> predicate(x) end)
+    |> length()
+  end
+
+  defp predicate({:ok, int}), do: int |> Integer.is_odd()
+  defp predicate({:error, _e}), do: false
+
+  defp read_integer(str) do
+    try do
+      str
+      |> String.to_integer()
+      |> (fn x -> {:ok, x} end).()
+    rescue
+      e in ArgumentError -> {:error, e}
+    end
   end
 end
